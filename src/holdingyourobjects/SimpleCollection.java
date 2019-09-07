b9c647dab8a29a2557324a88fd2678b05718c392
Zhu_Wenxiang <zwx199288@163.com>
2019-09-07 21:46:50 +0800

    holding your objects

---
+++ src/holdingyourobjects/SimpleCollection.java
@@ -0,0 +1,19 @@
+package holdingyourobjects;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SimpleCollection {
+
+	public static void main(String[] args) {
+		Collection<Integer> c=new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			c.add(i);
+		}
+		for (Integer integer : c) {
+			System.out.print(integer+",");
+		}
+
+	}
+
+}
